f5c960d870b9d86ad2e48126ff320e39da59153d
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2020-10-31 19:23:52 +0100

    Impostati tutti gli array ed oggetti, inclusi i prompt
    Trovato un modo per aggiungere un oggetto ad un array, con .push, ma non è quello che vorrei usare

--- main.js
+++ main.js
@@ -6,5 +6,106 @@
 
 $(document).ready(function() {
 
+// Creo un oggetto che rappresenta uno studente:
+
+    var student = {
+
+        'Name': 'Claudio',
+        'Surname': 'Borromeo',
+        'Age': '26',
+
+    };
+
+// Con un ciclo for in stampo a schermo le sue proprietà
+
+    for (var i = 0; i < student.length; i++) {
+        array[i]
+    };
+
+    for (var key in student) {
+
+        $('.first-student').append([key] + ': ');
+        $('.first-student').append(student[key] + ' | ');
+
+    };
+
+    var students = [
+
+        {
+            'Name' : 'Mario',
+            'Surname' : 'Rossi',
+            'Age' : '23',
+        },
+
+        {
+            'Name' : 'Paolo',
+            'Surname' : 'Bianchi',
+            'Age' : '30',
+        },
+
+        {
+            'Name' : 'Maria',
+            'Surname' : 'Neri',
+            'Age' : '33',
+        },
+
+        {
+            'Name' : 'Francesca',
+            'Surname' : 'Verdi',
+            'Age' : '19',
+        },
+
+    ];
+
+
+
+// Creo 3 prompt in cui chiedo all'utente nome cognome ed età
+
+    var userName = prompt('Inserisci il nome di uno studente');
+
+    var userSurname = prompt('Inserisci il cognome di uno studente');
+
+    var userAge = prompt('Inserisci l\'età di uno studente');
+
+// Versione con push
+
+    students.push({
+        'Name' : userName,
+        'Surname' : userSurname,
+        'Age' : userAge,
+    })
+
+// Versione con quello che abbiam studiato in classe
+
+    students.Name = userName;
+    students.Surname = userSurname;
+    students.Age = userAge;
+
+    console.log(students);
+
+    // Creo un ciclo for, dove scorro ogni singolo oggetto dell'array students
+
+        for (var i = 0; i < students.length; i++) {
+
+    // Variabile di supporto per selezionare un singolo oggetto dell'array
+            var currentStudent = students[i];
+
+            for (var key in currentStudent) {
+
+    // Se la variabile "key" è uguale alla proprietà "Name" o "Surname" stampo in pagina
+                if (key == 'Name') {
+
+                        $('.other-student').append('<p class="margin-top-20">' + key + ': ' + currentStudent['Name'] + '</p>');
+
+                } else if (key == 'Surname') {
+
+                    $('.other-student').append('<p>' + key + ': ' + currentStudent['Surname'] + '</p>');
+
+                }
+            }
+
+
+        }
+
 
 });
